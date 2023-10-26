ad6427a66c262e86a8b8cddfc7c1024a3350b7a5
Iroro Orife <iorife@netflix.com>
2023-10-25 22:46:15 -0700

    [ADD] date stamp as an argument

--- iroro_gitlog.sh
+++ iroro_gitlog.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+last_date_time_stamp=$1
 for d in ~/stash/*/.git; do
     r=${d%.git}
     # echo "$r"
     git -C "$r" log \
         --committer=iorife \
-        --after=2023-08-15 \
+        --after=$last_date_time_stamp \
         --reverse \
 	--pretty='format:%h%C(Cyan)  %cd%C(Green)  %s'
 done
